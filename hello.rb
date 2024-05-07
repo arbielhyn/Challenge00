9401bf55f6b4003d5bca53eb8ef4a3b4646e1be2
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-05-07 14:08:36 -0500

    second commit

--- hello.rb
+++ hello.rb
@@ -1,7 +1,9 @@
 # Arbie
 
 # Dollar Amount
-subtotal = 5.00
+puts "Enter Subtotal:"
+input = gets.chomp
+subtotal = input.to_f
 
 # Taxes
 gst_rate = 0.07
@@ -19,3 +21,12 @@ puts "Subtotal: $#{subtotal}"
 puts "PST: $#{pst} - #{pst_rate * 100}%"
 puts "GST: $#{gst} - #{gst_rate * 100}%"
 puts "Grand Total: $#{grand_total}"
+
+# Messages based on Output
+if grand_total <= 5
+    puts "Pocket Change"
+elsif grand_total < 20
+    puts "Wallet Time"
+else
+    puts "Charge It"
+end
